2bfe0b97834e2a88a4b9ea0874b5c77a0130f5b6
BleuRaven <xavierloux.loux@gmail.com>
2019-06-10 16:51:08 +0200

    update for test
    - New: You can now change the CollisionTraceFlag of the StaticMesh (Collision Complexity)
    - New: It is now possible to choose in the name of the root bone for the skeleton in addon preferences (if "Armature" Ue4 will remove the root bone)
    - New: You can check export_ExportOnlySelected = True for export only the selected assets groups
    - New: You can now chose what python intergration use for Import Script. ( /!\ With vania python integration some features like StaticMesh Lod or SkeletalMesh Sockets integration do not work )
    - New: You can now chose a presets for nomenclature export properties 
    - Change: The all collision are now named with two suffix. Type_MeshName_##
    - Change: new potential errors will be detected in the potential error report(CheckArmatureMultipleRoots)
    - Fixed: in 2.8 the Addon can try to export removed mesh and crash
    - Fixed: set clipboard with encode('utf8') do error

---
+++ blender-for-unrealengine - 2.8/blender-for-unrealengine/bfu_Basics.py
@@ -0,0 +1,119 @@
+#====================== BEGIN GPL LICENSE BLOCK ============================
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.	 If not, see <http://www.gnu.org/licenses/>.
+#  All rights reserved.
+#
+#======================= END GPL LICENSE BLOCK =============================
+
+import bpy
+import os
+import string
+import bmesh
+from mathutils import Vector
+from mathutils import Quaternion
+
+def ChecksRelationship(arrayA, arrayB):
+	#Checks if it exits an identical variable in two lists
+
+	for a in arrayA:
+		for b in arrayB:
+			if a == b:
+				return True
+	return False
+
+def GetChilds(obj):
+	#Get all direct childs of a object
+
+	ChildsObj = []
+	for childObj in bpy.data.objects:
+		pare = childObj.parent
+		if pare is not None:
+			if pare.name == obj.name:
+				ChildsObj.append(childObj)
+
+	return ChildsObj
+
+def getFirstDeformBoneParent(bone):
+	if bone.parent is not None:
+		if bone.use_deform == True:
+			return bone
+		else:
+			return getFirstDeformBoneParent(bone.parent)
+	return bone
+
+def SetCollectionUse(collection):
+	#Set if collection is hide and selectable
+	print(collection)
+	collection.hide_viewport = False
+	collection.hide_select = False
+	try:
+		bpy.context.view_layer.layer_collection.children[collection.name].hide_viewport = False
+	except:
+		print(collection.name," not found in view_layer.layer_collection")
+		pass
+
+def GetRecursiveChilds(obj):
+	#Get all recursive childs of a object
+	
+	saveObjs = []
+	
+	def tryAppend(obj):
+		if obj.name in bpy.context.scene.objects:
+			saveObjs.append(obj)
+
+	for newobj in GetChilds(obj):
+		for childs in GetRecursiveChilds(newobj):
+			tryAppend(childs)
+		tryAppend(newobj)
+	return saveObjs
+	
+def ConvertToConvexHull(obj):
+	#Convert obj to Convex Hull
+	mesh = obj.data
+	if not mesh.is_editmode:
+		bm = bmesh.new()
+		bm.from_mesh(mesh) #Mesh to Bmesh
+		acb = bmesh.ops.convex_hull(bm, input=bm.verts, use_existing_faces=True)
+		#acb = bmesh.ops.recalc_face_normals(bm, faces=bm.faces)
+		bm.to_mesh(mesh) #BMesh to Mesh
+
+def VerifiDirs(directory):
+	#Check and create a folder if it does not exist
+
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+
+
+def ValidFilename(filename):
+	# remove not allowed characters 
+	
+	valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+	filename = ''.join(c for c in filename if c in valid_chars)		
+	return filename
+
+	
+def ResetArmaturePose(obj):
+	#Reset armature pose
+
+	for x in obj.pose.bones:
+		x.rotation_quaternion = Quaternion((0,0,0),0)
+		x.rotation_euler = Vector((0,0,0))
+		x.scale = Vector((1,1,1))
+		x.location = Vector((0,0,0))
+		
+def setWindowsClipboard(text):
+	bpy.context.window_manager.clipboard = text
+	#bpy.context.window_manager.clipboard.encode('utf8') 
+	
+	
