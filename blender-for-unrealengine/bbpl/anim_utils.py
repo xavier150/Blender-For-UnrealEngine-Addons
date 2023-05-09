0baa50bd950537bb166bfc83cec331c02b37d1a4
BleuRaven <xavierloux.loux@gmail.com>
2023-05-09 19:07:43 +0200

    Add new NLA propert from 3.3 and fix Proxy FCurve use the wrong propertys

--- blender-for-unrealengine/bbpl/anim_utils.py
+++ blender-for-unrealengine/bbpl/anim_utils.py
@@ -102,8 +102,11 @@ class ProxyCopy_NlaStrip():
         for fcurve in nla_strip.fcurves:
             self.fcurves.append(ProxyCopy_FCurve(fcurve))
         self.frame_end = nla_strip.frame_end
+        if bpy.app.version >= (3, 3, 0):
+            self.frame_end_ui = nla_strip.frame_end_ui
         self.frame_start = nla_strip.frame_start
-        self.use_animated_influence = nla_strip.use_animated_influence
+        if bpy.app.version >= (3, 3, 0):
+            self.frame_start_ui = nla_strip.frame_start_ui
         self.influence = nla_strip.influence
         self.modifiers = nla_strip.modifiers  # TO DO
         self.mute = nla_strip.mute
@@ -113,6 +116,14 @@ class ProxyCopy_NlaStrip():
         self.select = nla_strip.select
         self.strip_time = nla_strip.strip_time
         # self.strips = strip.strips #TO DO
+        self.type = nla_strip.type
+        self.use_animated_influence = nla_strip.use_animated_influence
+        if bpy.app.version >= (3, 0, 0):
+            self.use_animated_time = nla_strip.use_animated_time
+            self.use_animated_time_cyclic = nla_strip.use_animated_time_cyclic
+            self.use_auto_blend = nla_strip.use_auto_blend
+            self.use_reverse = nla_strip.use_reverse
+            self.use_sync_length = nla_strip.use_sync_length
 
     def PasteDataOn(self, nla_strip: bpy.types.NlaStrip):
         # nla_strip.action = strip.action
@@ -123,12 +134,16 @@ class ProxyCopy_NlaStrip():
         nla_strip.blend_out = self.blend_out
         nla_strip.blend_type = self.blend_type
         nla_strip.extrapolation = self.extrapolation
-        # nla_strip.fcurves = self.fcurves
+        for i, fcurve in enumerate(self.fcurves):
+            new_fcurve = nla_strip.fcurves.find(fcurve.data_path)  # Can't create so use find
+            fcurve.PasteDataOn(new_fcurve)
         nla_strip.frame_end = self.frame_end
-        # nla_strip.frame_start = self.frame_start
-        nla_strip.use_animated_influence = self.use_animated_influence
+        if bpy.app.version >= (3, 3, 0):
+            nla_strip.frame_end_ui = self.frame_end_ui
+        nla_strip.frame_start = self.frame_start
+        if bpy.app.version >= (3, 3, 0):
+            nla_strip.frame_start_ui = self.frame_start_ui
         nla_strip.influence = self.influence
-
         # nla_strip.modifiers = self.modifiers #TO DO
         nla_strip.mute = self.mute
         # nla_strip.name = self.name
@@ -137,9 +152,14 @@ class ProxyCopy_NlaStrip():
         nla_strip.select = self.select
         nla_strip.strip_time = self.strip_time
         # nla_strip.strips = self.strips #TO DO
-        for i, fcurve in enumerate(self.fcurves):
-            new_fcurve = nla_strip.fcurves.find(fcurve.data_path)
-            fcurve.PasteDataOn(new_fcurve)
+        # nla_strip.type = self.type  # Read only
+        nla_strip.use_animated_influence = self.use_animated_influence
+        if bpy.app.version >= (3, 0, 0):
+            nla_strip.use_animated_time = self.use_animated_time
+            nla_strip.use_animated_time_cyclic = self.use_animated_time_cyclic
+            nla_strip.use_auto_blend = self.use_auto_blend
+            nla_strip.use_reverse = self.use_reverse
+            nla_strip.use_sync_length = self.use_sync_length
 
 
 class ProxyCopy_FCurve():
@@ -147,56 +167,10 @@ class ProxyCopy_FCurve():
     # It used to do a safe for copy the struct.
 
     def __init__(self, fcurve: bpy.types.FCurve):
-        self.action = fcurve.action
-        self.action_frame_end = fcurve.action_frame_end
-        self.action_frame_start = fcurve.action_frame_start
-        self.active = fcurve.active
-        self.blend_in = fcurve.blend_in
-        self.blend_out = fcurve.blend_out
-        self.blend_type = fcurve.blend_type
-        self.extrapolation = fcurve.extrapolation
-        self.fcurves = fcurve.fcurves
-        self.frame_end = fcurve.frame_end
-        self.frame_end_ui = fcurve.frame_end_ui
-        self.frame_start = fcurve.frame_start
-        self.frame_start_ui = fcurve.frame_start_ui
-        self.influence = fcurve.influence
-        self.modifiers = fcurve.modifiers
-        self.mute = fcurve.mute
-        self.name = fcurve.name
-        self.repeat = fcurve.repeat
-        self.scale = fcurve.scale
-        self.select = fcurve.select
-        self.strip_time = fcurve.strip_time
-        self.strips = fcurve.strips
-        self.type = fcurve.type
-        self.use_animated_influence = fcurve.use_animated_influence
-        self.use_animated_time = fcurve.use_animated_time
-        self.use_animated_time_cyclic = fcurve.use_animated_time_cyclic
-        self.use_auto_blend = fcurve.use_auto_blend
-        self.use_reverse = fcurve.use_reverse
-        self.use_sync_length = fcurve.use_sync_length
+        self.data_path = fcurve.data_path
 
     def PasteDataOn(self, fcurve: bpy.types.FCurve):
-        if fcurve:
-            fcurve.array_index = self.array_index
-            fcurve.color = self.color
-            fcurve.color_mode = self.color_mode
-            # fcurve.data_path = self.data_path
-            # fcurve.driver = self.driver  #TO DO
-            fcurve.extrapolation = self.extrapolation
-            fcurve.group = self.group
-            fcurve.hide = self.hide
-            # fcurve.is_empty = self.is_empty
-            # fcurve.is_valid = self.is_valid
-            # fcurve.keyframe_points = self.keyframe_points
-            fcurve.lock = self.lock
-            # fcurve.modifiers = self.modifiers #TO DO
-            fcurve.mute = self.mute
-            # fcurve.sampled_points = self.sampled_points
-            fcurve.select = self.select
-            for keyframe_point in self.keyframe_points:
-                fcurve.keyframe_points.insert(keyframe_point.co[0], keyframe_point.co[1])
+        pass
 
 
 class AnimationManagment():
