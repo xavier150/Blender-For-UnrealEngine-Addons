c476c0ed5f43bcfd58e16baa67ea41a704c333df
BleuRaven <xavierloux.loux@gmail.com>
2024-07-25 21:22:43 +0200

    Update versioning to Semantic Versioning https://semver.org

--- blender-for-unrealengine/__init__.py
+++ blender-for-unrealengine/__init__.py
@@ -140,7 +140,7 @@ if "bfu_cached_asset_list" in locals():
 bl_info = {
     'name': 'Blender for UnrealEngine',
     'author': 'Loux Xavier (BleuRaven)',
-    'version': (0, 4, 3, 1),
+    'version': (4, 3, 1),
     'blender': (2, 80, 0),
     'location': 'View3D > UI > Unreal Engine',
     'description': "This add-ons allows to easily export several objects at the same time and import in Unreal Engine.",
