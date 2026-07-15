ca5f3d3b0560900cdbd5efb1fa4170bb12fb95f6
Xavier Loux <xavierloux.loux@gmail.com>
2026-07-15 23:28:15 +0200

    Test 5.2 + New: Support Blender 5.2.

---
+++ ReleaseLogs/Version_4.4.7.md
@@ -0,0 +1,5 @@
+# Unreal Engine Assets Exporter - Release Log
+Release Logs: https://github.com/xavier150/Blender-For-UnrealEngine-Addons/wiki/Release-Logs
+
+### Version 4.4.6
+- New: Support Blender 5.2.
--- bfu_builder/config.py
+++ bfu_builder/config.py
@@ -30,6 +30,7 @@ is_linux = platform.system() == "Linux"
 
 if is_linux:
     # Linux paths
+    add_blender_version((5, 2, 0), Path(linux_blender_install_path / "blender-5.2.0-linux-x64/blender"))
     add_blender_version((5, 1, 2), Path(linux_blender_install_path / "blender-5.1.2-linux-x64/blender"))
     add_blender_version((5, 0, 1), Path(linux_blender_install_path / "blender-5.0.1-linux-x64/blender"))
     add_blender_version((4, 5, 1), Path(linux_blender_install_path / "blender-4.5.1-linux-x64/blender"))
@@ -55,7 +56,8 @@ if is_linux:
     add_blender_version((2, 80, 0), Path(linux_blender_install_path / "blender-2.80rc3-linux-glibc217-x86_64/blender"))
 elif is_windows:
     # Windows paths
-    add_blender_version((5, 1, 0), Path(windows_blender_install_path / "Blender 5.1/blender.exe"))
+    add_blender_version((5, 2, 0), Path(windows_blender_install_path / "Blender 5.2/blender.exe"))
+    add_blender_version((5, 1, 2), Path(windows_blender_install_path / "Blender 5.1/blender.exe"))
     add_blender_version((5, 0, 1), Path(windows_blender_install_path / "Blender 5.0/blender.exe"))
     add_blender_version((4, 5, 0), Path(windows_blender_install_path / "Blender 4.5/blender.exe"))
     add_blender_version((4, 4, 0), Path(windows_blender_install_path / "Blender 4.4/blender.exe"))
